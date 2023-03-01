d369c67efe291e6ac31ff8e0a44581969a4d201d
QuitX <Glebar1437@yandex.ru>
2023-03-01 14:57:42 +0300

    basic task manager

---
+++ ToDo/routes.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, render_template, url_for,redirect
+from ToDo.models import ToDo, db
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+    app.config['SQLALCHEMY_TRACK_NOTIFICATIONS'] = False
+
+    db.init_app(app)
+
+    return app 
+
+app = create_app()
+
+
+
+@app.get('/')
+def home():
+    todo_list = ToDo.query.all()
+    return render_template('ToDo/index.html', todo_list=todo_list, title="Main page")
+
+@app.post('/add')
+def add():
+    title = request.form.get('title')
+    new_todo = ToDo(title=title, is_complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for('home'))
+   
+@app.get('/update/<int:todo_id>')
+def update(todo_id):
+    todo = ToDo.query.filter_by(id=todo_id).first()
+    todo.is_complete = not todo.is_complete
+    db.session.commit()
+    return redirect(url_for('home'))
+
+@app.get('/delete/<int:todo_id>')
+def delete(todo_id):
+    todo = ToDo.query.filter_by(id=todo_id).first() 
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for('home'))
